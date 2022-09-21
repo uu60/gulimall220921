41c6729537b516c02229030ad6da44057a324160
uu60 <djz6660@icloud.com>
2022-09-21 19:25:38 +0800

    init project

---
+++ ware/src/test/java/com/octopus/gulimall/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octopus.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
